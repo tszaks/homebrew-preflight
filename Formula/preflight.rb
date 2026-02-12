06429769cb4b47ef8271245abc8b2cb4a951b2e5
Tyler Szakacs <tylerszakacs@gmail.com>
2026-02-12 13:21:52 -0500

    chore: bump formula to v1.0.7

--- Formula/preflight.rb
+++ Formula/preflight.rb
@@ -1,8 +1,8 @@
 class Preflight < Formula
   desc "App Store Review Scanner CLI - catch rejection reasons before Apple does"
   homepage "https://preflightlaunch.com"
-  url "https://registry.npmjs.org/preflightlaunch/-/preflightlaunch-1.0.6.tgz"
-  sha256 "da1393c62cfd1640e656dad00af706fe15a2994b748399f38649627a1164ec25"
+  url "https://registry.npmjs.org/preflightlaunch/-/preflightlaunch-1.0.7.tgz"
+  sha256 "0b6d28c9f2fce901bfd0e0c1b7e23fba41e0310e3d7b97153bd205baab00d43d"
   license "MIT"
 
   depends_on "node@22"
@@ -13,6 +13,6 @@ class Preflight < Formula
   end
 
   test do
-    assert_match "1.0.6", shell_output("#{bin}/preflight --version")
+    assert_match "1.0.7", shell_output("#{bin}/preflight --version")
   end
 end
